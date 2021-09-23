c5d37c14261fa03490c523937bbad45b328ccd0e
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-23 14:11:03 -0400

    Added new and create methods in Bulk Discount controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -7,4 +7,28 @@ class BulkDiscountsController < ApplicationController
 
   def show
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = @merchant.bulk_discounts.create(bulk_discount_params)
+    if bulk_discount.save
+      flash.notice = 'Success! A new bulk discount was created.'
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else
+      flash.alert = "Incorrect Information. Please submit again."
+      redirect_to new_merchant_bulk_discount_path(@merchant)
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.required(:bulk_discount).permit(:percentage_discount, :quantity_threshold)
+  end
+
 end
