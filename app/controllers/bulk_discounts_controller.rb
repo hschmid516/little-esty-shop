d9c914bb4d9610eefbb784c918e1c495c9cf96bb
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-23 13:34:46 -0400

    Added access to facade to call @holidays in controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,8 +1,10 @@
 class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    holiday_facade = BulkDiscountsFacade.new
+    @holidays = holiday_facade.holiday_api
   end
 
   def show
-  end 
+  end
 end
