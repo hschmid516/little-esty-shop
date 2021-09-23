9b73630bd61dc5cc562a5fc8cc0a17716c7c5cd7
Henry Schmid <henry.schmid1@gmail.com>
2021-09-23 17:31:38 -0600

    Add feature to edit existing discount

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,18 +1,23 @@
 class DiscountsController < ApplicationController
   before_action :find_merchant
-  before_action :find_discount, only: [:show, :destroy]
+  before_action :find_discount, except: [:index, :create, :new]
 
   def index
     @holidays = DiscountFacade.new.holiday_api
   end
 
   def new
-    @discount = Discount.new(params[:id])
+    @discount = Discount.new
   end
 
   def create
     @merchant.discounts.create(discount_params)
     redirect_to merchant_discounts_path(@merchant)
+  end
+
+  def update
+    @discount.update(discount_params)
+    redirect_to merchant_discount_path(@merchant, @discount)
   end
 
   def destroy
