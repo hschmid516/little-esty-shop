8204c0c91db56529c592bce5d244627d4150126a
Henry Schmid <henry.schmid1@gmail.com>
2021-09-23 14:45:33 -0600

    Add feature to create new discount

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,6 +1,25 @@
 class DiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new, :create]
   def index
-    @merchant = Merchant.find(params[:merchant_id])
     @holidays = DiscountFacade.new.holiday_api
+  end
+
+  def new
+    @discount = Discount.new(params[:id])
+  end
+
+  def create
+    @merchant.discounts.create(discount_params)
+    redirect_to merchant_discounts_path(@merchant)
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:name, :percentage, :threshold)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
   end
 end
