e9456a788ec753209591a041938e7a5d697fdc71
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-24 09:44:55 -0400

    Write a method in bulk discounts to find the best possible discount per quantity

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,10 @@ class BulkDiscount < ApplicationRecord
 
   validates :quantity_threshold, presence: true
   validates :percentage_discount, presence: true
+
+  def self.max_discount(quantity)
+    where('quantity_threshold <= ?', quantity)
+      .order(percentage_discount: :desc)
+      .first
+  end
 end
