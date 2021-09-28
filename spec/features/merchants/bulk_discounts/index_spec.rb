0157576297b5912381d3597c842efe1356a843c8
Henry Schmid <henry.schmid1@gmail.com>
2021-09-28 15:28:28 -0600

    Add feature and model tests for deleting discount with pending items

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe 'merchant bulk discounts index' do
   before :each do
     @merch1 = create(:merchant)
     @merch2 = create(:merchant)
-    @disc1 = @merch1.discounts.create(name: 'Fall Special', percentage: 0.25, threshold: 2)
-    @disc2 = @merch1.discounts.create(name: 'Super Saver', percentage: 0.50, threshold: 3)
-    @disc3 = @merch2.discounts.create(name: 'Other Discount', percentage: 0.50, threshold: 3)
-    json = File.read('spec/fixtures/holidays.json')
+    @cust1 = create(:customer)
+    @item1 = create(:item, merchant: @merch1)
+    @item2 = create(:item, merchant: @merch1)
+    @item3 = create(:item, merchant: @merch1)
+    @invoice1 = create(:invoice, customer: @cust1)
+    @invoice2 = create(:invoice, customer: @cust1)
+    @ii1 = InvoiceItem.create(item: @item1, invoice: @invoice1, status: 0, quantity: 10, unit_price: 1000)
+    @ii2 = InvoiceItem.create(item: @item2, invoice: @invoice1, status: 1, quantity: 20, unit_price: 4000)
+    @ii3 = InvoiceItem.create(item: @item3, invoice: @invoice2, status: 0, quantity: 30, unit_price: 1000)
+    @disc1 = @merch1.discounts.create(name: 'Fall Special', percentage: 0.25, threshold: 20)
+    @disc2 = @merch1.discounts.create(name: 'Super Saver', percentage: 0.40, threshold: 35)
+    @disc3 = @merch2.discounts.create(name: 'Other Discount', percentage: 0.50, threshold: 5)
+    @json = File.read('spec/fixtures/holidays.json')
     stub_request(:get, "https://date.nager.at/api/v2/NextPublicHolidays/us").
-      to_return(status: 200, body: json)
+      to_return(status: 200, body: @json)
     visit merchant_discounts_path(@merch1)
   end
 
@@ -44,7 +53,10 @@ RSpec.describe 'merchant bulk discounts index' do
       within('h1') do
         expect(page).to have_content('Upcoming Holidays')
       end
-      expect(page).to have_content('h')
+
+      expect(page).to have_content('Columbus Day Date: 2021-10-11')
+      expect(page).to have_content('Veterans Day Date: 2021-11-11')
+      expect(page).to have_content('Thanksgiving Day Date: 2021-11-25')
     end
   end
 
@@ -55,14 +67,27 @@ RSpec.describe 'merchant bulk discounts index' do
   end
 
   it 'can delete a discount' do
-    expect(page).to have_content(@disc1.name)
+    expect(page).to have_content(@disc2.name)
+
+    within("#discount-#{@disc2.id}") do
+      click_link('Delete')
+    end
+
+    expect(current_path).to eq(merchant_discounts_path(@merch1))
+    expect(page).to_not have_content(@disc2.name)
+  end
+
+  it 'only deletes discount if no pending invoice_items' do
+    Discounter.call(@invoice1)
+    Discounter.call(@invoice2)
 
     within("#discount-#{@disc1.id}") do
       click_link('Delete')
     end
 
     expect(current_path).to eq(merchant_discounts_path(@merch1))
-    expect(page).to_not have_content(@disc1.name)
+    expect(page).to have_content(@disc1.name)
+    expect(page).to have_content("This discount is applied to an item that is pending - Can't delete discount!")
   end
 end
 
