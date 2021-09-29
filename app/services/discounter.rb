3543e7c712150c3161954bb0f1cb784abc3eabbe
Henry Schmid <henry.schmid1@gmail.com>
2021-09-28 20:21:08 -0600

    Update discounter to only add discount if invoice is completed

--- app/services/discounter.rb
+++ app/services/discounter.rb
@@ -10,7 +10,7 @@ class Discounter
   def apply_discount
     @invoice.applicable_discounts.each do |invoice_item|
       ii = InvoiceItem.find_by_id(invoice_item.id)
-      if ii.discount.nil?
+      if ii.discount.nil? && @invoice.status == 'completed'
         ii.update(discount: invoice_item.percentage, discount_id: invoice_item.discount_id)
       end
     end
