bee76dd595cdb42cf37f515041de38b75499059e
Henry Schmid <henry.schmid1@gmail.com>
2021-09-28 20:20:43 -0600

    Add test for adding II disocunt if invoice status changes to completed

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -9,7 +9,7 @@ describe 'admin invoices show page' do
     @item2 = create(:item, merchant: @merch1, unit_price: 6000)
     @item3 = create(:item, merchant: @merch1, unit_price: 4500)
     @item4 = create(:item, merchant: @merch2, unit_price: 1000)
-    @invoice1 = create(:invoice, customer: @cust1, status: 0)
+    @invoice1 = create(:invoice, customer: @cust1, status: 2)
     @ii1 = InvoiceItem.create(item: @item1, invoice: @invoice1, status: 1, quantity: 15, unit_price: 1000)
     @ii2 = InvoiceItem.create(item: @item2, invoice: @invoice1, status: 1, quantity: 9, unit_price: 2000)
     @ii3 = InvoiceItem.create(item: @item3, invoice: @invoice1, status: 1, quantity: 25, unit_price: 3000)
@@ -65,7 +65,7 @@ describe 'admin invoices show page' do
     expect(page).to have_content("Total Revenue: $5,080.00")
   end
 
-  it 'has a select option for invoice status' do
+  it 'can update invoice status' do
     within("#invoice-#{@invoice1.id}-status") do
       select 'completed', from: :invoice_status
       click_button('Update Invoice Status')
@@ -91,5 +91,36 @@ describe 'admin invoices show page' do
 
   it 'shows total discounted revenue' do
     expect(page).to have_content("Total Discounted Revenue: $4,242.50")
+  end
+
+  it 'updates invoice item discount field when set to completed' do
+    @merch1 = create(:merchant)
+    @merch2 = create(:merchant)
+    @cust1 = create(:customer)
+    @item1 = create(:item, merchant: @merch1, unit_price: 3000)
+    @item2 = create(:item, merchant: @merch1, unit_price: 6000)
+    @item3 = create(:item, merchant: @merch1, unit_price: 4500)
+    @item4 = create(:item, merchant: @merch2, unit_price: 1000)
+    @invoice1 = create(:invoice, customer: @cust1, status: 0)
+    @ii1 = InvoiceItem.create(item: @item1, invoice: @invoice1, status: 1, quantity: 15, unit_price: 1000)
+    @ii2 = InvoiceItem.create(item: @item2, invoice: @invoice1, status: 1, quantity: 9, unit_price: 2000)
+    @ii3 = InvoiceItem.create(item: @item3, invoice: @invoice1, status: 1, quantity: 25, unit_price: 3000)
+    @ii4 = InvoiceItem.create(item: @item4, invoice: @invoice1, status: 1, quantity: 100, unit_price: 4000)
+    @disc3 = @merch1.discounts.create(name: 'Mad deals', percentage: 0.75, threshold: 20)
+    @disc1 = @merch1.discounts.create(name: 'Fall Special', percentage: 0.25, threshold: 10)
+    @disc2 = @merch1.discounts.create(name: 'Super Saver', percentage: 0.50, threshold: 15)
+    @disc4 = @merch2.discounts.create(name: 'Lame Deal', percentage: 0.05, threshold: 100)
+    visit admin_invoice_path(@invoice1)
+
+    expect(@ii1.discount).to eq(nil)
+
+    within("#invoice-#{@invoice1.id}-status") do
+      select 'completed', from: :invoice_status
+      click_button('Update Invoice Status')
+    end
+
+    @ii1.reload
+    
+    expect(@ii1.discount).to eq(0.5)
   end
 end
