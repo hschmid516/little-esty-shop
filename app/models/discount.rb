3f0807f8220014c2d3f1cbf62bcaa9d709b96dfe
Henry Schmid <henry.schmid1@gmail.com>
2021-09-28 21:40:04 -0600

    Update methods to only edit/update if discount is applicable

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -5,4 +5,8 @@ class Discount < ApplicationRecord
   def pending_invoice_items?
     !invoice_items.where(status: 0, discount_id: id).empty?
   end
+
+  def self.discount_applicable?(discount_params, merchant_id)
+    where('threshold <= ? AND percentage > ? AND merchant_id = ?', discount_params[:threshold], discount_params[:percentage], merchant_id).empty?
+  end
 end
