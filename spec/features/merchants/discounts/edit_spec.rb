fbb6f0de994a9b3fa4e3218aae3cc27865459b24
Henry Schmid <henry.schmid1@gmail.com>
2021-09-28 18:39:11 -0600

    Add test for not editing if invoice item is pending

--- spec/features/merchants/discounts/edit_spec.rb
+++ spec/features/merchants/discounts/edit_spec.rb
@@ -3,7 +3,19 @@ require 'rails_helper'
 RSpec.describe 'merchant bulk discounts edit page' do
   before :each do
     @merch1 = create(:merchant)
-    @disc1 = @merch1.discounts.create(name: 'Fall Special', percentage: 0.25, threshold: 2)
+    @cust1 = create(:customer)
+    @item1 = create(:item, merchant: @merch1)
+    @item2 = create(:item, merchant: @merch1)
+    @item3 = create(:item, merchant: @merch1)
+    @invoice1 = create(:invoice, customer: @cust1)
+    @invoice2 = create(:invoice, customer: @cust1)
+    @ii1 = InvoiceItem.create(item: @item1, invoice: @invoice1, status: 0, quantity: 10, unit_price: 1000)
+    @ii2 = InvoiceItem.create(item: @item2, invoice: @invoice1, status: 1, quantity: 20, unit_price: 4000)
+    @ii3 = InvoiceItem.create(item: @item3, invoice: @invoice2, status: 1, quantity: 30, unit_price: 1000)
+    @disc1 = @merch1.discounts.create(name: 'Fall Special', percentage: 0.25, threshold: 25)
+    @disc2 = @merch1.discounts.create(name: 'Super Saver', percentage: 0.40, threshold: 5)
+    Discounter.call(@invoice1)
+    Discounter.call(@invoice2)
     visit edit_merchant_discount_path(@merch1, @disc1)
   end
 
@@ -15,7 +27,6 @@ RSpec.describe 'merchant bulk discounts edit page' do
 
   it 'can edit some discount information' do
     fill_in('Name', with: 'Super Deal')
-
     click_button('Update Discount')
 
     expect(current_path).to eq(merchant_discount_path(@merch1, @disc1))
@@ -30,7 +41,6 @@ RSpec.describe 'merchant bulk discounts edit page' do
     fill_in('Name', with: 'Super Deal')
     fill_in('Discount Percentage', with: 0.5)
     fill_in('Quantity Threshold', with: 6)
-
     click_button('Update Discount')
 
     expect(current_path).to eq(merchant_discount_path(@merch1, @disc1))
@@ -41,5 +51,15 @@ RSpec.describe 'merchant bulk discounts edit page' do
       expect(page).to have_content(6)
       expect(page).to_not have_content(@disc1.name)
     end
+  end
+
+  it 'only edits discount if no pending invoice_items' do
+    visit edit_merchant_discount_path(@merch1, @disc2)
+    fill_in('Name', with: 'Super Deal')
+    click_button('Update Discount')
+
+    expect(current_path).to eq(edit_merchant_discount_path(@merch1, @disc2))
+    expect(page).to have_content("This discount is applied to an item that is pending - Can't edit discount!")
+    expect(page).to have_content('Super Saver')
   end
 end
